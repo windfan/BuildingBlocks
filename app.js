ffbd464cfc9bf201ace12f89d5270a47c40d0225
Victor <ryu1031@gmail.com>
2016-01-10 15:55:47 -0500

    Listing cities on HTML

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static('public'));
 
-
-app.get('/', function(request, response){
-	response.send('OK');
+app.get('/cities', function(request, response){
+	var cities = ['Lotopia', 'Caspiana', 'Indigo'];
+	response.json(cities);
 });
-
 
 module.exports = app;
