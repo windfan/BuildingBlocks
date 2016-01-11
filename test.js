82b9f9d1a5dd89d4ced0f1bfa3a90a25364d4484
Victor <ryu1031@gmail.com>
2016-01-10 19:02:12 -0500

    Replace in memory with Redis

--- test.js
+++ test.js
@@ -1,5 +1,10 @@
 var request = require('supertest');
 var app = require('./app');
+var redis = require('redis');
+var client = redis.createClient();
+client.select('test'.length);
+client.flushdb();
+
 
 describe('Requests to root path', function(){
 
@@ -54,13 +59,17 @@ describe('Listing cities on /cities', function(){
 
 		request(app)
 			.get('/cities')
-			.expect(JSON.stringify(['Lotopia', 'Caspiana', 'Indigo']), done);
+			.expect(JSON.stringify([]), done);
 
 	});
 
 });
 
 describe('Creating new cities', function(){
+
+	before(function(){
+
+	});
 
 	it('Returns a 201 status code', function(done){
 
